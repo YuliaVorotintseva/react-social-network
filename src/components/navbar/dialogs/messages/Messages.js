bdfbf8784de8e30df822d4f47ffd09690b09898b
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2020-05-30 22:54:55 +0600

    add react-redux

--- src/components/navbar/dialogs/messages/Messages.js
+++ src/components/navbar/dialogs/messages/Messages.js
@@ -6,28 +6,24 @@ import MessageContainer from '../../../../ui/MessageContainer'
 import StoreContext from '../../../../StoreContext'
 
 const Messages = () => (
-    <StoreContext.Consumer>
-        {
-            value => (
-                <div className={styleClasses.Messages}>
-                {
-                    value.state.dialogs.messages.map(({message, avatar, id}) => <Message
-                            key={id}
-                            message={message}
-                            avatar={avatar}
-                        />
-                    )
-                }
+    <div className={styleClasses.Messages}>
+        <StoreContext.Consumer>
+            {
+                value => value.state.dialogs.messages.map(({message, avatar, id}) => <Message
+                    key={id}
+                    message={message}
+                    avatar={avatar}
+                    />
+                )
+            }
+        </StoreContext.Consumer>
 
-                <hr />
-                <MessageContainer
-                    updateActionCreator={updateMessageActionCreator}
-                    addActionCreator={sendMessageActionCreator}
-                />
-            </div>
-            )
-        }
-    </StoreContext.Consumer>
+        <hr />
+        <MessageContainer
+            updateActionCreator={updateMessageActionCreator}
+            addActionCreator={sendMessageActionCreator}
+        />
+    </div>
 )
 
 export default Messages
