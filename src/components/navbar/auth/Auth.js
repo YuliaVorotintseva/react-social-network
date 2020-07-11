e8204e3d6c62e6a2a2a46ed5b0973575fee07daf
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2020-07-11 17:15:39 +0600

    add Auth validation

--- src/components/navbar/auth/Auth.js
+++ src/components/navbar/auth/Auth.js
@@ -2,11 +2,91 @@ import React from 'react'
 import styleClasses from './Auth.module.css'
 import Button from '../../ui/button/Button'
 import Input from '../../ui/input/Input'
+import is from 'is_js'
 
 export default class Auth extends React.Component {
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Enter correct email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Enter correct password',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
     loginHandler = () => {}
     authHandler = () => {}
+
+    onChangeHandler = (event, controlName) => {
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        let isFormValid = true
+        Object.keys(formControls).forEach(name => isFormValid = formControls[name].valid && isFormValid)
+
+        formControls[controlName] = control
+        this.setState({
+            isFormValid,
+            formControls
+        })
+    }
+
+    validateControl = (value, validation) => {
+        if(!validation) {
+            return true
+        }
+
+        let isValid = true
+        if(validation.required) isValid = value.trim() !== '' && isValid
+        if(validation.email) isValid = is.email(value) && isValid
+        if(validation.minLength) isValid = value.length >= validation.minLength && isValid
+        return isValid
+    }
+
     render() {
+        const inputs = Object.keys(this.state.formControls)
+            .map((controlName, index) => {
+                const control = this.state.formControls[controlName]
+                return (
+                    <Input
+                        key={controlName + index}
+                        type={control.type}
+                        value={control.value}
+                        valid={control.valid}
+                        touched={control.touched}
+                        label={control.label}
+                        shouldValidate={!!control.validation}
+                        errorMessage={control.errorMessage}
+                        onChange={e => this.onChangeHandler(e, controlName)}
+                    />
+                )
+            })
+        
         return (
             <div className={styleClasses.Auth}>
                 <div>
@@ -15,10 +95,21 @@ export default class Auth extends React.Component {
                         onSubmit={e => e.preventDefault()}
                         className={styleClasses.Form}
                     >
-                        <Input label='Email'/>
-                        <Input label='Password'/>
-                        <Button onClick={this.loginHandler}>Login</Button>
-                        <Button onClick={this.authHandler}>Auth</Button>
+                        {inputs}
+                        
+                        <Button
+                            onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Login
+                        </Button>
+
+                        <Button
+                            onClick={this.authHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Auth
+                        </Button>
                     </form>
                 </div>
             </div>
